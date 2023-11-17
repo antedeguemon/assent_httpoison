4c20165c310d0708ebe4c2b9776a6955078e8ffd
antedeguemon <antedeguemon@users.noreply.github.com>
2023-11-17 00:30:32 -0500

    Adds docs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,9 @@ defmodule AssentHTTPoison.MixProject do
     [
       app: :assent_httpoison,
       version: "0.1.0",
+      description: "Assent adapter for making requests using HTTPoison",
+      package: package(),
+      docs: docs(),
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,8 +24,23 @@ defmodule AssentHTTPoison.MixProject do
     [
       {:assent, ">= 0.1.11"},
       {:bandit, "~> 1.1", only: :test},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:httpoison, "~> 1.8.0"},
       {:test_server, "~> 0.1.13", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/antedeguemon/assent_httpoison"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
